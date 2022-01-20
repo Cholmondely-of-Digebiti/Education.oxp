081f1c4f1d5a23f5729f063dc2a8e08da97ec7fc
Cholmondely-of-Digebiti <95695363+Cholmondely-of-Digebiti@users.noreply.github.com>
2022-01-20 09:38:24 +0000

    Update Education_Station_Bar.js
    Added Spara's crucial comma and Montana's overlay: (replacing "background:" which gets overridden by XenonUI.oxp).

--- Education copy.txt/Scripts/Education_Station_Bar.js
+++ Education copy.txt/Scripts/Education_Station_Bar.js
@@ -2,12 +2,12 @@ this.name           = "Education_Station_Bar.js";
 this.author         = "Cholmondely";
 this.copyright      = "(C) 2022 Cholmondely";
 this.licence        = "CC-NC-by-SA 4.0";
-this.description    = "This OXP does not yet do very much.";
-this.version        = "0.1.1";
+this.description    = "Adds a station bar where you might overhear useful tips";
+this.version        = "0.0.9";
 
 "use strict";
 
-// Having a "Visit the Station Bar" on the F4 Screen might be a way to go for that. This script would let you give a randomly picked item each time. Although you might want to also add a timer so the messages change over time. You could also add tests for Government type, economy or station type so different items are overheard in different systems types and station types.
+// LittleBear's comments: This script would let you give a randomly picked item each time. Although you might want to also add a timer so the messages change over time. You could also add tests for Government type, economy or station type so different items are overheard in different systems types and station types.
 
 // Set Up your F4 Screen Option Like this:-
 
@@ -18,7 +18,7 @@ this.removebar = this.shipWillLaunchFromStation = function() {
 player.ship.dockedStation.setInterface("bar_chatter",null);
 }
 
-//  Now Add your Visit the Bar Interface like this:- 
+//  Now add your Visit the Bar Interface like this:- 
 
 this.barinterface = function() {	
 player.ship.dockedStation.setInterface("bar_chatter",{
@@ -34,10 +34,8 @@ this.showBar = function() {
 mission.runScreen({
 title: "The Local Bar",
 screenID:"show_bar",
-var text = expandDescription("[education_bar_gossip]");
-message: 
-"... anyway, as the jump countdown hit one, some nutter in a Ferdie ran right across my bow. I pulled up hard on the 'stick to avoid the bastard... next thing I know, the bloody klaxon's wailing, and I'm in ghastly glowing green interstellar space surrounded by gooks!";
-background: "litf_bg_localbar.png", //This adds the bar image behind the message
-
+message: expandDescription("[education_bar_gossip]"),
+overlay: "litf_bg_localbar.png", //This adds the bar image behind the message
+                  })
 }
 
